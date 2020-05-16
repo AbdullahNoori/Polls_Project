65c55dbe632c5bdd9a58a9c260c21bdab9cabb94
AbdullahNoori <nooriabdullah86@gmail.com>
2020-05-15 19:36:00 -0700

    updates

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,9 +22,6 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('registration/', include('registration.urls')),
-
+        path('', include('polls.urls')),
+        path('', include('polls.urls')),
 ]
